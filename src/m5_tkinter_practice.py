b292d5f168e28aa04f7f95ee91aeb4f80b2ab071
wilsonjt <wilsonjt@rose-hulman.edu>
2019-01-31 14:43:47 -0500

    Finished!

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -3,44 +3,60 @@ This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Jack Wilson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
+def print_number_of_times(string, n):
+    """Prints input a number of times"""
+    for _ in range(n):
+        print(string)
 
 
 def main():
     """ Constructs a GUI with stuff on it. """
     # -------------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DONE: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # -------------------------------------------------------------------------
+    window = tkinter.Tk()
 
     # -------------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DONE: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # -------------------------------------------------------------------------
-
+    frame = ttk.Frame(window, padding=10)
+    frame.grid()
     # -------------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DONE: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # -------------------------------------------------------------------------
+    go_forward_button = ttk.Button(frame, text='Forward')
+    go_forward_button.grid()
 
     # -------------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # DONE: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # -------------------------------------------------------------------------
+    hello_button = ttk.Button(frame, text='Hello')
+    hello_button['command'] = (lambda: print('Hello'))
+    hello_button.grid()
 
     # -------------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DONE: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # -------------------------------------------------------------------------
+    input_box = ttk.Entry(frame)
+    input_box.grid()
+    print_input_button = ttk.Button(frame, text='Print input')
+    print_input_button['command'] = (lambda: print(input_box.get()))
+    print_input_button.grid()
 
     # -------------------------------------------------------------------------
     # TODO: 7.
@@ -65,11 +81,20 @@ def main():
     #      s = entry_box.get()
     #      n = int(s)
     ####################################################################
+    x = 0
+    number_box = ttk.Entry(frame)
+    number_box.grid()
+    number_button = ttk.Button(frame, text='No. of repeats')
+    number_button['command'] = (lambda:
+                                print_number_of_times(input_box.get(), int(number_box.get())))
+    number_button.grid()
 
     # -------------------------------------------------------------------------
     # TODO: 8. As time permits, do other interesting GUI things!
     # -------------------------------------------------------------------------
+    window.mainloop()
 
+    print('Done with the Event Loop')  # Note when this line runs
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
